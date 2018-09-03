f15129117a045e658d29fa10675a510f35895d1d
Johann Bich <johann.bich@axa.co.jp>
2018-09-03 14:57:09 +0900

    clean up

--- company-api/src/main/java/com/kalote/companyapi/repositories/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kalote.companyapi.repositories;
-
-import com.kalote.companyapi.models.Company;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CompanyRepository extends MongoRepository<Company, String> {
-  Company findBy_id(ObjectId _id);
-}
--- employee-api/error.js
+++
@@ -1,4 +0,0 @@
-export default function errorHandler(err, res) {
-  console.log('Something went wrong! ', err);
-  res.status(500).send(err);
-}
